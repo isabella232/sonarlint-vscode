f732f8d0d6b0998f319dd9f60e1b1cc9f7e21167
sojaph <jafaridze.sofo@gmail.com>
2022-06-08 15:06:15 +0200

    SLVSCODE-296 wip, get conections list from configuration

--- src/connections.ts
+++ src/connections.ts
@@ -4,46 +4,52 @@ export class Connection extends VSCode.TreeItem {
     constructor(
         public readonly id: string,
         public readonly label: string,
-        public readonly collapsibleState: VSCode.TreeItemCollapsibleState,
         public readonly contextValue: 'sonarqubeConnection' | 'sonarcloudConnection'
     ) {
-        super(label, collapsibleState);
+        super(label, VSCode.TreeItemCollapsibleState.None);
     }
+    collapsibleState = VSCode.TreeItemCollapsibleState.None;
 }
 
 export class ConnectionGroup extends VSCode.TreeItem {
     constructor(
         public readonly id: 'sonarqube' | 'sonarcloud',
-        public readonly label: string,
-        public readonly collapsibleState: VSCode.TreeItemCollapsibleState,
+        public readonly label: 'SonarQube' | 'SonarCloud',
         public readonly contextValue: 'sonarQubeGroup' | 'sonarCloudGroup'
     ) {
-        super(id, collapsibleState)
+        super(label, VSCode.TreeItemCollapsibleState.Collapsed)
     }
 }
 
 
-export type AllConnectionsNode = Connection | ConnectionGroup;
+export type ConnectionsNode = Connection | ConnectionGroup;
 
 export interface ConnectionsResponse {
     'sonarqube': Array<Connection>;
     'sonarcloud': Array<Connection>;
 }
 
-export function listAllConnections(): ConnectionsResponse {
+function listAllConnections(): ConnectionsResponse {
     return {
-        'sonarqube': [new Connection('Cat', '1.0',VSCode.TreeItemCollapsibleState.None,'sonarqubeConnection')],
-        'sonarcloud': [new Connection('Dog','21.0', VSCode.TreeItemCollapsibleState.None,'sonarcloudConnection')]
+        'sonarqube': getConnections('sonarqube'),
+        'sonarcloud': getConnections('sonarcloud')
     }
 }
 
-export class AllConnectionsTreeDataProvider implements VSCode.TreeDataProvider<AllConnectionsNode> {
+function getConnections(type: string) : Connection[] {
+    const contextValue = type === 'sonarqube' ? 'sonarqubeConnection' : 'sonarcloudConnection';
+    const labelKey = type === 'sonarqube' ? 'serverUrl' : 'organizationKey';
+    let connections = VSCode.workspace.getConfiguration('sonarlint.connectedMode.connections')[type];
+    connections = connections.map(c => new Connection(c['connectionId'], c[labelKey], contextValue))
+    return connections;
+}
+
+export class AllConnectionsTreeDataProvider implements VSCode.TreeDataProvider<ConnectionsNode> {
 
     private readonly _onDidChangeTreeData = new VSCode.EventEmitter<Connection | undefined>();
     readonly onDidChangeTreeData: VSCode.Event<Connection | undefined> = this._onDidChangeTreeData.event;
 
     constructor() { }
-
 
     refresh() {
         this._onDidChangeTreeData.fire(null);
@@ -53,22 +59,18 @@ export class AllConnectionsTreeDataProvider implements VSCode.TreeDataProvider<A
         return element;
     }
 
-    getChildren(element?: AllConnectionsNode): AllConnectionsNode[] {
+    getChildren(element?: ConnectionsNode): ConnectionsNode[] {
         if(!element){
             return this.getInitialState()
         }
-        const cr = listAllConnections();
-        if(element.id === 'sonarqube'){
-            return cr.sonarqube
-        } else if(element.id === 'sonarcloud'){
-            return cr.sonarcloud
-        }
+        const connectionsResponse = listAllConnections();
+        return connectionsResponse[element.id]
     }
 
     getInitialState() : ConnectionGroup[] {
         return [
-            new ConnectionGroup('sonarqube', 'SonarQube Connections', VSCode.TreeItemCollapsibleState.Collapsed, 'sonarQubeGroup'),
-            new ConnectionGroup('sonarcloud', 'SonarCloud Connections', VSCode.TreeItemCollapsibleState.Collapsed, 'sonarCloudGroup')
+            new ConnectionGroup('sonarqube', 'SonarQube', 'sonarQubeGroup'),
+            new ConnectionGroup('sonarcloud', 'SonarCloud', 'sonarCloudGroup')
         ]
     }
 }
